67abc25f5a05ff4320a2db02d8a666b10565b441
Sunidhikini15 <155661546+Sunidhikini15@users.noreply.github.com>
2026-02-19 18:14:12 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,17 @@
+// Last updated: 2/19/2026, 6:14:12 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> hash;
+        int res=0;
+        int majority=0;
+        for(int n:nums){
+            hash[n]=1+hash[n];
+            if(hash[n]>majority){
+                res=n;
+                majority=hash[n];
+            }
+        }
+        return res;
+    }
+};
