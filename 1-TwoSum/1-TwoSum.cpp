9f6c8cb980fe05e5a855206f5a9612a10383c619
Sunidhikini15 <155661546+Sunidhikini15@users.noreply.github.com>
2026-02-19 18:16:14 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,18 @@
+// Last updated: 2/19/2026, 6:16:14 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> numMap;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            numMap[nums[i]]=i;
+        }
+        for(int i=0;i<n;i++){
+            int complement=target-nums[i];
+            if(numMap.count(complement) && numMap[complement]!=i){
+                return{i,numMap[complement]};
+            }
+        }
+        return{};
+    }
+};
