2b745a2278b88602c5b7894c0bf69cc5e1a14e6a
Sunidhikini15 <155661546+Sunidhikini15@users.noreply.github.com>
2026-02-19 18:15:01 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,18 @@
+// Last updated: 2/19/2026, 6:15:01 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char,char> mapping={{')','('},{']','['},{'}','{'}};
+        for(char c:s){
+            if(mapping.find(c)==mapping.end()){
+                stack.push(c);
+            }else if(!stack.empty()&&mapping[c]==stack.top()){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+};
